d739567765cf5bfc2dbf920a5f2cdec1934aeeb8
RezeScarlet <rezescarlet@gmail.com>
2026-01-12 20:23:34 -0300

    Created React + Tailwind + Shadcn

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React + Shadcn</h1>
+          <div className="flex min-h-svh flex-col items-center justify-center">
+              <Button>Click me</Button>
+          </div>
+
+    </>
+  )
+}
+
+export default App
