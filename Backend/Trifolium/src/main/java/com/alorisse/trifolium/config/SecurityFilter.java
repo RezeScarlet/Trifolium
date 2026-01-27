86945503f2e23326971d2572cabe2c4e58186a1e
RezeScarlet <rezescarlet@gmail.com>
2026-01-27 17:12:46 -0300

    Created and work in progress

--- Backend/Trifolium/src/main/java/com/alorisse/trifolium/config/SecurityFilter.java
+++ Backend/Trifolium/src/main/java/com/alorisse/trifolium/config/SecurityFilter.java
@@ -1,4 +1,48 @@
 package com.alorisse.trifolium.config;
 
-public class SecurityFilter {
+import com.alorisse.trifolium.model.entity.User;
+import com.alorisse.trifolium.repository.UserRepository;
+import com.alorisse.trifolium.service.TokenService;
+import com.alorisse.trifolium.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserService userService;
+
+    public SecurityFilter(TokenService tokenService, UserService userService) {
+        this.tokenService = tokenService;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverTokenRequest(request);
+
+        if (token != null) {
+            var login = tokenService.validateToken(token);
+
+            if (login != null) {
+                User user = UserRepository.findByEmail(login).orElseThrow(() -> new RuntimeException("User not found"));
+            }
+        }
+
+
+    }
+
+    private String recoverTokenRequest(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+
+    }
 }
